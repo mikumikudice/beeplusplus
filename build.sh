4da9bc2347f31385bc8b2f60bf1fabb37eeac6c7
mateusmds <mateusmoraisdias3@gmail.com>
2021-09-20 18:06:21 -0300

    + new compiler flag (ignore formating warnings)

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 set -e
-gcc -g -Wall -Wno-dangling-else -Wno-unused-variable ./src/bi.c -o ./bin/bi -fsanitize=leak,address
+gcc -g -Wall -Wno-dangling-else -Wno-format -Wno-unused-variable ./src/bi.c -o ./bin/bi -fsanitize=leak,address
