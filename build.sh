10b436a2d524b4c81939ca9ef777d68f711b8268
mateusmds <mateusmoraisdias3@gmail.com>
2022-02-09 16:48:52 -0300

    + added another flag to debug compilation

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 set -e
 mkdir -p ./bin/debug
-gcc -g -Wall -Wno-dangling-else -Wno-format -Wno-unused-variable ./src/bi.c -o ./bin/debug/bi -fsanitize=leak,address
+gcc -g -Wall -Wno-dangling-else -Wno-discarded-qualifiers -Wno-format -Wno-unused-variable ./src/bi.c -o ./bin/debug/bi -fsanitize=leak,address
