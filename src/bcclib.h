e4573293e5a367cc474b67fb259c030fe519b23a
mateusmds <mateusmoraisdias3@gmail.com>
2022-03-17 18:15:14 -0300

    - removed c++ garbage from bcclib

--- src/bcclib.h
+++ src/bcclib.h
@@ -1,6 +1,6 @@
 #define bcclib
 
-// Check if string is empty
+// checks if string is empty
 #define isblnk(val) (!strcmp(val, ""))
 
 #define arrlen(arr)  sizeof(arr) / sizeof(arr[0])
@@ -8,17 +8,12 @@
 #define max(n, o) (n > o ? n : o)
 #define min(n, o) (n > o ? o : n)
 
-#ifndef __cplusplus
+// better operators
+#define and &&
+#define or  ||
 
-    #define and && // Better operators
-    #define or  || // Better operators
+// Fortran-like boolean
+typedef enum boolean {F, T} bool;
 
-    // Fortran-like boolean
-    typedef enum boolean {F, T} bool;
-
-    // nil for C
-    #define nil ((void *) 0)
-#else
-    // nil for C++
-    #define nil __null
-#endif
+// nil for C
+#define nil ((void *) 0)
